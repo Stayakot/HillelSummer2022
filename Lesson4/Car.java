da94b76be12b6912fd89c1ff8fb34c1ade916262
Stayakot <pentagram.secret@gmail.com>
2022-05-30 23:05:18 +0300

    HomeWork4

---
+++ Lesson4/Car.java
@@ -0,0 +1,59 @@
+package Lesson4;
+
+public class Car {
+    double volume;
+    double remainFuel;
+    double consumption100km;
+    double requiredToBunker;
+
+    public Car(double volume, double remainFuel, double consumption100km) {
+        this.volume = volume;
+        this.remainFuel = remainFuel;
+        this.consumption100km = consumption100km;
+    }
+
+    public void fullVolume(){
+        if (remainFuel<volume){
+            double bunker=volume-remainFuel;
+            remainFuel+=bunker;
+            System.out.println("Для полного бака нужно залить: "+bunker);
+            System.out.println("Количество топлива после заправки: " + remainFuel + " лтр.");
+        }
+    }
+
+    public void remainFuelDuringDistance(double distance){
+        double consumption = distance/100*consumption100km;
+        System.out.println("Расход за " + distance+ "км. :" + consumption+ " лтр.");
+        remainFuel-=consumption;
+        System.out.println("Остаток: "+ remainFuel);
+
+    }
+
+    public void requiredFuelForDistance(double distance){
+        double requiredFuel=distance/100*consumption100km;
+        requiredToBunker = requiredFuel-remainFuel;
+        System.out.println("Для преодоленния " + distance + " км."+ " Понадобится: " + requiredFuel + " лтр. топлива");
+        System.out.println("Дозаправить нужно: " + requiredToBunker+ " лтр.");
+
+    }
+
+}
+
+class Main{
+    public static void main(String[] args) {
+
+        Car car = new Car(30, 29.5, 15);
+        car.requiredFuelForDistance(481);
+        double price=Integer.parseInt(args[0])*car.requiredToBunker;
+        System.out.println("Стоимость топлива что нужно дозаправить: " + price);
+        car.remainFuelDuringDistance(178);
+        System.out.println("Остаток топлива на  Кривое Озеро: " + car.remainFuel);
+        car.fullVolume();
+        car.remainFuelDuringDistance(155);
+        System.out.println("Остаток топлива на  Жашков: " + car.remainFuel);
+        car.fullVolume();
+        car.remainFuelDuringDistance(148);
+        System.out.println("Остаток топлива на  Киев: " + car.remainFuel);
+
+    }
+}
