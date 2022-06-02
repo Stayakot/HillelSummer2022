2e77b497c832e14add5049e7f78ab994125b88ea
Stayakot <pentagram.secret@gmail.com>
2022-06-02 23:12:09 +0300

    Updated HomeWork5

--- Lesson5/Array.java
+++ Lesson5/Array.java
@@ -3,23 +3,34 @@ package Lesson5;
 public class Array {
     public static void main(String[] args) {
 
+        Student Misha = new Student(9, "Misha", "Muron", "Nataliev", 1995, "Odesa", "568684868", "Math", 1, "A");
+        Student Petya = new Student(10, "Petya", "Petrov", "Petrovovich", 1990, "Lviv", "26569968", "History", 1, "B");
+        Student Olya = new Student(8, "Olya", "Klevna", "Mihailovna", 1996, "Odesa", "5564648", "Economic", 2, "C");
+        Student Katya = new Student(7, "Katya", "Korovna", "Bukova", 1991, "Kiev", "5651575", "Economic", 4, "B");
+        Student Alena = new Student(6, "Alena", "Popova", "Kirova", 1994, "Lviv", "5646546804", "Math", 4, "A");
+        Student Kostya = new Student(5, "Kostya", "Rubin", "Alexceev", 1996, "Odesa", "3584890", "History", 1, "B");
+        Student Vlad = new Student(4, "Vlad", "Slyvka", "Olegovych", 1993, "Odesa", "35489080", "Math", 4, "A");
+        Student Sasha = new Student(3, "Sasha", "Korankov", "Mishuta", 1995, "Kiev", "1541656486", "Math", 1, "C");
+        Student Oleg = new Student(2, "Oleg", "Polets", "Konstantinovich", 1995, "Odesa", "380921535", "History", 3, "B");
+        Student Kristina = new Student(1, "Kristina", "Spivak", "Alexandrovna", 1994, "Odesa", "+38095543", "Math", 3, "A");
 
         Student[] student = new Student[10];
-        student[0] = new Misha(9, "Misha", "Muron", "Nataliev", 1995, "Odesa", "568684868", "Math", 1, "A");
-        student[1] = new Petya(10, "Petya", "Petrov", "Petrovovich", 1990, "Lviv", "26569968", "History", 1, "B");
-        student[2] = new Olya(8, "Olya", "Klevna", "Mihailovna", 1996, "Odesa", "5564648", "Economic", 2, "C");
-        student[3] = new Katya(7, "Katya", "Korovna", "Bukova", 1991, "Kiev", "5651575", "Economic", 4, "B");
-        student[4] = new Alena(6, "Alena", "Popova", "Kirova", 1994, "Lviv", "5646546804", "Math", 4, "A");
-        student[5] = new Kostya(5, "Kostya", "Rubin", "Alexceev", 1996, "Odesa", "3584890", "History", 1, "B");
-        student[6] = new Vlad(4, "Vlad", "Slyvka", "Olegovych", 1993, "Odesa", "35489080", "Math", 4, "A");
-        student[7] = new Sasha(3, "Sasha", "Korankov", "Mishuta", 1995, "Kiev", "1541656486", "Math", 1, "C");
-        student[8] = new Oleg(2, "Oleg", "Polets", "Konstantinovich", 1995, "Odesa", "380921535", "History", 3, "B");
-        student[9] = new Kristina(1, "Kristina", "Spivak", "Alexandrovna", 1994, "Odesa", "+38095543", "Math", 3, "A");
+        student[0] = Misha;
+        student[1] = Petya;
+        student[2] = Olya;
+        student[3] = Katya;
+        student[4] = Alena;
+        student[5] = Kostya;
+        student[6] = Vlad;
+        student[7] = Sasha;
+        student[8] = Oleg;
+        student[9] = Kristina;
 
         Methods.faculty(student, "Math");
         Methods.studentCheckFacultyAndCourse(student, "History", 3);
         Methods.year(student, 1991);
         Methods.listGroup(student, "A");
+
 
     }
 }
--- Lesson5/Student.java
+++ Lesson5/Student.java
@@ -5,13 +5,13 @@ public class Student {
     private int id;
     private String name;
     private String surname;
-    private  String middleName;
-    private   int year;
-    private  String address;
-    private  String telNumber;
+    private String middleName;
+    private int year;
+    private String address;
+    private String telNumber;
     private String faculty;
-    private  int course;
-    private   String group;
+    private int course;
+    private String group;
 
     public int getId() {
         return id;
@@ -126,86 +126,4 @@ public class Student {
                 ", group='" + group + '\'' +
                 '}';
     }
-}
-class Petya extends Student {
-    Petya() {
-    }
-
-    public Petya(int id, String name, String surname, String middleName, int year, String address, String telNumber, String faculty, int course, String group) {
-        super(10, "Petya", "Petrov", "Petrovovich", 1990, "Lviv", "26569968", "History", 1, "B");
-    }
-}
-
-class Misha extends Student {
-    Misha() {
-    }
-    public Misha(int id, String name, String surname, String middleName, int year, String address, String telNumber, String faculty, int course, String group) {
-        super(9, "Misha", "Muron", "Nataliev", 1995, "Odesa", "568684868", "Math", 1, "A");
-    }
-}
-
-
-class Olya extends Student {
-    Olya() {
-    }
-    public Olya(int id, String name, String surname, String middleName, int year, String address, String telNumber, String faculty, int course, String group) {
-        super(8, "Olya", "Klevna", "Mihailovna", 1996, "Odesa", "5564648", "Economic", 2, "C");
-    }
-}
-
-class Katya extends Student {
-    Katya() {
-    }
-    public Katya(int id, String name, String surname, String middleName, int year, String address, String telNumber, String faculty, int course, String group) {
-        super(7, "Katya", "Korovna", "Bukova", 1991, "Kiev", "5651575", "Economic", 4, "B");
-    }
-}
-
-class Alena extends Student {
-    Alena() {
-    }
-    public Alena(int id, String name, String surname, String middleName, int year, String address, String telNumber, String faculty, int course, String group) {
-        super(6, "Alena", "Popova", "Kirova", 1994, "Lviv", "5646546804", "Math", 4, "A");
-    }
-}
-
-class Kostya extends Student {
-    Kostya() {
-    }
-    public Kostya(int id, String name, String surname, String middleName, int year, String address, String telNumber, String faculty, int course, String group) {
-        super(5, "Kostya", "Rubin", "Alexceev", 1996, "Odesa", "3584890", "History", 1, "B");
-    }
-}
-
-class Vlad extends Student {
-    Vlad() {
-    }
-    public Vlad(int id, String name, String surname, String middleName, int year, String address, String telNumber, String faculty, int course, String group) {
-        super(4, "Vlad", "Slyvka", "Olegovych", 1993, "Odesa", "35489080", "Math", 4, "A");
-    }
-}
-
-class Sasha extends Student {
-    Sasha() {
-    }
-    public Sasha(int id, String name, String surname, String middleName, int year, String address, String telNumber, String faculty, int course, String group) {
-        super(3, "Sasha", "Korankov", "Mishuta", 1995, "Kiev", "1541656486", "Math", 1, "C");
-    }
-}
-
-class Oleg extends Student {
-    Oleg() {
-    }
-    public Oleg(int id, String name, String surname, String middleName, int year, String address, String telNumber, String faculty, int course, String group) {
-        super(2, "Oleg", "Polets", "Konstantinovich", 1995, "Odesa", "380921535", "History", 3, "B");
-    }
-}
-
-class Kristina extends Student {
-    Kristina() {
-    }
-    public Kristina(int id, String name, String surname, String middleName, int year, String address, String telNumber, String faculty, int course, String group) {
-        super(1, "Kristina", "Spivak", "Alexandrovna", 1994, "Odesa", "+38095543", "Math", 3, "A");
-    }
-
 }
