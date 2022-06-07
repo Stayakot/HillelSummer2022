ddd1024bc6bde26d720a4c3c68721a15f792343c
Stayakot <pentagram.secret@gmail.com>
2022-06-07 07:28:10 +0300

    HomeWork6

--- Lesson6/SubscriberService.java
+++ Lesson6/SubscriberService.java
@@ -6,11 +6,20 @@ public interface SubscriberService {
     //сведения об абонентах, у которых время внутригородских разговоров превышает заданное;
     void localTime(Subscriber[] subscribers, int localTime) ;
 
+
+    //сведения об абонентах, которые пользовались междугородней связью;
     public  void international(Subscriber[] subscribers) ;
 
+
+    //сведения об абонентах ФИО, номер телефона, баланс у который первая буква фамилии передается как параметр.
     public  void firstLetter(Subscriber[] subscribers, char firstLetter) ;
+
+
+    // суммарное потребление трафика интернета для определенного города
     public  void internetTriffic(Subscriber[] subscribers, String city) ;
 
+
+    //количество абонентов с отрицательны балансом
     public  void balance(Subscriber[] subscribers) ;
 
 }
